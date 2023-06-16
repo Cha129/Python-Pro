6e20ceb03f06ff40d4785ff13759eef77804389e
Cha129 <136828221+Cha129@users.noreply.github.com>
2023-06-16 20:51:12 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,53 +1,15 @@
-import speech_recognition as sr
-import pyttsx3
-import pywhatkit
-import datetime
-import wikipedia
-listener=sr.Recognizer()
-engine=pyttsx3.init()
-print("OK")
-def talk(text):
-    engine.say(text)
-    engine.runAndWait()
-def take_command():
-    try:
-        with sr.Microphone() as source:
-            talk("Hi my dear boss chanakya i am chandragupta i am hapy to meet you once again in 2022")
-            print('listening')
-            audio=listener.listen(source)
-            query=listener.recognize_google(audio)
-    except:
-        print('sorry')
-    return query
-
-
-def run_chandragupta():
-    print('give me command')
-    command=take_command()
-    print("THIS IS WORKING NICE")
-    if "play" in command:
-        song = command.replace('play', "")
-        talk('playing' + song)
-        pywhatkit.playonyt(song)
-    if "time" in command:
-        time = datetime.datetime.now().strftime("%I:%H %p")
-        talk("Current time is" + time)
-    if "who is " in command:
-        person = command.replace("who is", '')
-        info = wikipedia.summary(person, 1)
-        print(info)
-        talk(info)
-run_chandragupta()
-
-
-
-
-
-
-
-
-
-
-
-
+import cv2
+import cvzone
+cap = cv2.VideoCapture(0)
+cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+filter=input("which filter do you want from following options:sunglass.png,beard.png,""cool.png,pirate.png,star.png:")
+overlay=cv2.imread(filter,cv2.IMREAD_UNCHANGED)
+while True:
+    _,frame = cap.read()
+    faces=cascade.detectMultiScale(frame)
+    for (x,y,w,h) in faces:
+        frame=cvzone.overlayPNG(frame,overlay)
+    cv2.imshow('snapit', frame)
+    if cv2.waitKey(10) == ord('q'):
+        break
 
